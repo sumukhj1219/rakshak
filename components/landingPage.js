9fadbc2d1a31ffdbbc2c78a13f6c2b441cf3ba58
sumukhj1219 <sumukhjoshi75@gmail.com>
2024-12-19 18:08:53 +0530

    loaded images

--- components/landingPage.js
+++ components/landingPage.js
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-center mx-auto'>
+      <Image
+      src={'/black-ops.jpg'}
+      width={1920}
+      height={1024}
+      alt='background'
+      className='object-cover brightness-50 hover:brightness-90 transition-all duration-1000'
+      />
     </div>
   )
 }
